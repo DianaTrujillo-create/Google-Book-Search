11ae30df982b4fb84d583848e878541b8be39c7c
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-05-20 19:44:34 -0500

    app css

---
+++ google-books-search/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const mainHeader = () => {
+    return (
+      <div className= 'main-image d-flex justify-content-center align-item-center flex-column'>
+        {/* Overlay */}
+        <div className="filter"></div>
+      </div>
+    )
+  }
+  return (
+    <div>
+      start
+    </div>
+  );
+}
+
+export default App;
