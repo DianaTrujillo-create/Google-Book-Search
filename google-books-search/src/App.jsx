4a5637eb6b9c69ce2a3f733f589d9927414c6204
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-05-20 22:24:27 -0500

    commit to main

--- google-books-search/src/App.jsx
+++ google-books-search/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
+import {InputGroup, Input, InputGroupAddon} from 'reactstrap';
 
 function App() {
   const mainHeader = () => {
     return (
-      <div className= 'main-image d-flex justify-content-center align-item-center flex-column'>
+      <div className= 'main-image d-flex justify-content-center align-items-center flex-column'>
         {/* Overlay */}
-        <div className="filter"></div>
+        <div className='filter'></div>
+        <h1 className='display-2 text-center text-white mb-3' style={{zIndex:2}}>
+          Google Books
+        </h1>
+        <div style={ { width: '60%', zIndex: 2 } }>
+          <InputGroup size='lg' className='mb-3'>
+            <Input placeholder='Book Search'/>
+            <InputGroupAddon addonType='append'>
+              <button color='secondary'>
+                <i className='fas fa-search'></i>
+              </button>  
+            </InputGroupAddon>
+          </InputGroup>
+          
+        </div>
       </div>
     )
   }
