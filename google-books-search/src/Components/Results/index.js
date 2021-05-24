4e885a6d035ee33b83a35a1e4c34e885e4fc0af9
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-05-24 15:16:43 -0500

    Update index.js

--- google-books-search/src/Components/Results/index.js
+++ google-books-search/src/Components/Results/index.js
@@ -28,7 +28,28 @@ class Results extends Component {
     render() {
         return (
             <div>
-                {!this.}
+                {!this.props.books.lenght ? (
+                    <h1 className="text-center">No results to Display</h1>
+                ) : (
+                    <div>
+                        {this.props.books.map(result=> (
+                            <div className="card mb-3" key={result._id}>
+                                <div className="row">
+                                    <div className="col-md-2">
+                                        <img alt={result.title} className="img-fluid" src={result.image} />
+                                    </div>
+                                    <div className="col-md-10">
+                                        <div className="card-body">
+                                            <h5 className="card-title">{result.title} by {result.authors}</h5>
+                                            <p className="card-text">{result.description}</p>
+                                            <div>
+                                                <a href={result.link} className="btn badge-pill"
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        ))}
+                )}
             </div>
         )
     }
